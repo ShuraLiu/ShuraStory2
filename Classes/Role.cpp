9bb72f7232c4eb4847b9d2f8c8f1d1a3378621dc
LiuXiaoyi <liuxiaoyi135@gmail.com>
2014-12-20 00:20:37 +0800

    添加角色类

---
+++ Classes/Role.cpp
@@ -0,0 +1,19 @@
+//
+//  Role.cpp
+//  ShuraStory
+//
+//  Created by 刘潇逸 on 14/12/18.
+//
+//
+
+#include "Role.h"
+
+Role::Role()
+{
+    
+}
+
+Role::~Role()
+{
+    
+}
---
+++ Classes/Role.h
@@ -0,0 +1,21 @@
+//
+//  Role.h
+//  ShuraStory
+//
+//  Created by 刘潇逸 on 14/12/18.
+//
+//
+
+#ifndef __ShuraStory__Role__
+#define __ShuraStory__Role__
+
+#include <stdio.h>
+
+class Role
+{
+public:
+    Role();
+    virtual ~Role();
+};
+
+#endif /* defined(__ShuraStory__Role__) */
